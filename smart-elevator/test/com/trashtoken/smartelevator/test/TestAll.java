87be6e881942739ab8ae09ba9de99b9c29793670
Weiming cai <trashtoken@live.com>
2012-01-14 07:19:38 +0800

    myeclipse10 used

---
+++ smart-elevator/test/com/trashtoken/smartelevator/test/TestAll.java
@@ -0,0 +1,12 @@
+package com.trashtoken.smartelevator.test;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class TestAll {
+	@Test
+	public void testDate(){
+		System.out.println(new Date().getTime());
+	}
+}
